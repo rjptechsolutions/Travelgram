ec76c0e1c9ce2f3895c272978fda1c88e0a7b8aa
Rohan Joshi <rohaanjosh@gmail.com>
2020-12-12 18:34:01 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyA43hrOciUl0Wzk9ekcJYMvohTNkcVeUWs',
-    authDomain: 'gitseach-77ec1.firebaseapp.com',
-    databaseURL: 'https://gitseach-77ec1.firebaseio.com',
-    projectId: 'gitseach-77ec1',
-    storageBucket: 'gitseach-77ec1.appspot.com',
-    messagingSenderId: '927905897065',
-    appId: '1:927905897065:web:f7faa38a2e4e07296360f8',
-    measurementId: 'G-C52N68FB3R',
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
   },
 };
